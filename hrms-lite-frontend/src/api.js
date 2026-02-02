05614975696f343cd3252af05d1106b9493d7b7d
aakash malik <mraakashmalik@gmail.com>
2026-02-02 18:37:11 +0530

    c

--- hrms-lite-frontend/src/api.js
+++ hrms-lite-frontend/src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://hrms-lite-1-12ym.onrender.com",
+  baseURL: "https://hrms-lite-1-12ym.onrender.com"
 });
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+console.log("API:", BASE_URL);
